960447d9cfb0f950f9dde8acfb4375041d7c8d03
Tom Payne <twpayne@gmail.com>
2021-06-16 23:20:13 +0200

    Refactor ParseTask

--- xctrack.go
+++ xctrack.go
@@ -3,10 +3,19 @@ package xctrack
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
+)
+
+var (
+	errEmptyInput    = errors.New("empty input")
+	errInvalidFormat = errors.New("invalid format")
 )
 
 // ParseTask parses a Task from data.
 func ParseTask(data []byte) (*Task, error) {
+	if len(data) == 0 {
+		return nil, errEmptyInput
+	}
 	if bytes.HasPrefix(data, []byte(QRCodeScheme)) {
 		var qrCodeTask QRCodeTask
 		if err := json.Unmarshal(data[len(QRCodeScheme):], &qrCodeTask); err != nil {
@@ -15,8 +24,8 @@ func ParseTask(data []byte) (*Task, error) {
 		return qrCodeTask.Task(), nil
 	}
 	var task Task
-	if err := json.Unmarshal(data, &task); err != nil {
-		return nil, err
+	if err := json.Unmarshal(data, &task); err == nil {
+		return &task, nil
 	}
-	return &task, nil
+	return nil, errInvalidFormat
 }
