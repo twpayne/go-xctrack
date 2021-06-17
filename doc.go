2ba4f5978f3374693c9815cef84568dc87d1aabb
Tom Payne <twpayne@gmail.com>
2021-06-17 19:05:24 +0200

    Tidy up documentation

--- doc.go
+++ doc.go
@@ -1,2 +1,3 @@
-// Package xctrack interfaces with XCTrack, http://xctrack.org/.
+// Package xctrack implements XCTrack's task format. See http://xctrack.org/ and
+// http://xctrack.org/Competition_Interfaces.html.
 package xctrack
